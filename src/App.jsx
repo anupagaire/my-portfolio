963783250a0d39f37e70a2d29039659c85150a82
Anupa Gaire <aanupa.gaire@gmail.com>
2025-08-21 12:57:04 +0545

    achievement section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Achievements from './Component/Achievement.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Achievements />
+    </>
+  )
+}
+
+export default App
---
+++ src/achievementdata.js
@@ -0,0 +1,7 @@
+export const achievements = [
+  { year: "2019 - 2020", title: "Achievement 1" },
+  { year: "2020 - 2021", title: "Achievement 2" },
+  { year: "2021 - 2022", title: "Achievement 3" },
+  { year: "2022 - 2023", title: "Achievement 4" },
+  { year: "2023 - Present", title: "Achievement 5" }
+];
