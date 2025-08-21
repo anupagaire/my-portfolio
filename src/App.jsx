4e2433480d3ad95d78bc14b925ab7fa21178a8b2
Anupa Gaire <aanupa.gaire@gmail.com>
2025-08-21 20:45:59 +0545

    Updated package and app files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+   import Achievements from './Components/Achievements';
+import './App.css';
+import Home from "./Pages/Home";
+import Research from "./Pages/Research" 
+import Courses from "./Pages/Courses" 
+import Projectsupervisor from "./Pages/Projectsupervisor" 
+import Navbar from "./Components/Navbar.jsx";
+import React from 'react';
+import AdminLogin from './Components/AdminLogin.jsx';
+import AdminDashboard from './Components/AdminDashboard.jsx';
+   function App() {
+     return (
+       <Router>
+              <Navbar />
 
-import './App.css'
-import Achievements from './Component/Achievement.jsx'
+         <Routes>
+          <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/projectsupervisor" element={<Projectsupervisor />} />
+        <Route path="/research" element={<Research />} />
+           <Route path="/" element={<Achievements />} />
+           <Route path="/admin/login" element={<AdminLogin />} />
+           <Route path="/admin/dashboard" element={<AdminDashboard />} />
+           <Route path="*" element={<Achievements />} /> {/* Fallback to portfolio */}
+         </Routes>
+       </Router>
+     );
+   }
 
-function App() {
-
-  return (
-    <>
-      <Achievements />
-    </>
-  )
-}
-
-export default App
+   export default App;
