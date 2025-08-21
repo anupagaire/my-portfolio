bfb04508f6254dee163b248b6a222960bca6df09
Anupa Gaire <aanupa.gaire@gmail.com>
2025-08-21 20:42:13 +0545

    pages added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Achievements from '../Components/Achievements';
+
+const Home = () => {
+  return (
+    <section className="page-section">
+      <Achievements />
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Projectsupervisor.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projectsupervisor = () => {
+  return (
+    <section className="page-section">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is the Home page.</p>
+    </section>
+  );
+};
+
+export default Projectsupervisor;
