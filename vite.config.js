f3d3d0f33c2471924750dccc38dd8a98666d327b
Anupa Gaire <129235252+anupagaire@users.noreply.github.com>
2025-09-23 21:26:16 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/',          // 👈 IMPORTANT for custom domain
+  base: '/',          // 👈 IMPORTANT for custom domain
   plugins: [react()],
 })
